3f50b241596704ee3585bd36a17eefbe85a243d4
Lacoste Aymeric <aymeric-lacoste@hotmail.fr>
2017-02-27 21:24:10 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
 
 //////////////  DEBUT DU ROUND /////////////////////////////
     while($round <= 10){                                       ////// Tant que le 10eme round n'est pas terminé : 
-        echo '<p style="color:red">Round N°'.$round.' : </p>'; // Indique à quel round nous sommes
+        echo '<p style="color:red;font-size:20px">Round N°'.$round.' : </p>'; // Indique à quel round nous sommes
         $premiere_boule = rand(0,10);                          // Fait un premier lancé de boule random entre 0 et 10
         echo 'Resultat du lancé N° '.$boule.' : '.$premiere_boule.'</br></br>'; // Affiche le nombre de quilles tombées au premier lancé
         $boule = 2;                                            // Prepare le 2eme lancé
@@ -18,7 +18,7 @@
         if($premiere_boule == $gg){                            ////// Si le 1er lancé est directement un STRIKE : 
             if($bonus == 2 OR $bonus == 1){$cumul = $cumul+20;}// Si le round précédent était un strike ou un spare, ajoute 20 points
             else if($bonus == 0){$cumul = $cumul + 10;}        // Sinon Rajoute 10 points au score total
-            echo '<h3 style="color:blue">STRIKE ! 10 points ! '.' '.'Score total : </h3>'.$cumul; // Annonce le strike
+            echo '<h3 style="color:blue">STRIKE ! 10 points ! '.' '.'Score total : '.$cumul.'</h3>'; // Annonce le strike
             $round = $round+1;                                  // On passe au round suivant 
             $bonus = 2;
             $boule = 1;
@@ -31,7 +31,7 @@
             $variable = $premiere_boule*2;                     // Stock une variable qui double les points du lancé N°1, en cas de spare
             $restant = 10-$premiere_boule;                     // On calcule le nombre de quilles restantes
             $premiere_boule = rand(0,$restant);                // On fait un deuxieme lancé random
-            echo 'Resultat du lancé N° '.$boule.' : '.$premiere_boule.'</br></br>'; // Annonce le résultat du 2eme lancé
+            echo 'Resultat du lancé N° '.$boule.' : '.$premiere_boule.'</br>'; // Annonce le résultat du 2eme lancé
 
 
 ////////////    SI LES LANCES ONT FAIT SPARE    /////////////////
@@ -39,7 +39,7 @@
                 if($bonus == 1){$cumul = $cumul+$variable+$premiere_boule;}// Si le round précédent était un spare double points du 1er lancé
                 else if($bonus == 2){$cumul = $cumul+20;}                  // Si c'était un strike, ajoute 20 points
                 else{$cumul = $cumul + 10;}                          // Ajoute 10points au score total
-                echo '<h3 style="color:green">Spare ! 10 points ! '.' '.' Score total : </h3>'.$cumul; // Annonce le SPARE et le score total
+                echo '<h3 style="color:green">Spare ! 10 points ! '.' '.' Score total : '.$cumul.'</h3></br>'; // Annonce le SPARE et le score total
                 
                 $round = $round+1;                             // Passage au round suivant
                 $boule = 1;                                    // Le prochain lancé sera donc le 1er lancé du tour suivant
@@ -52,7 +52,7 @@
                 if($bonus == 1){$cumul = $cumul+$variable+$premiere_boule;}// Si le round précédent était un spare double points du 1er lancé
                 else if($bonus == 2){$cumul = $cumul+(($resultat+$premiere_boule)*2);}//Double les points en cas de strike le round précédent
                 else if($bonus == 0){$cumul = $cumul+$resultat+$premiere_boule;}   // Ajoute lancé 1 + lancé 2 au score total 
-                echo $resultat+$premiere_boule.' Score total : '.$cumul; // Annonce le score cumulé des deux lancé, et le score total
+                echo '<p style="color:violet">'.($resultat+$premiere_boule).' quilles ce tour. Score total : <span style="font-size:25px;">'.$cumul.'</span></p></br>'; // Annonce le score cumulé des deux lancé, et le score total
                 
                 $round = $round+1;                             // Passage au round suivant
                 $boule = 1;                                    // Le prochain lancé sera donc le 1er lancé du tour suivant
